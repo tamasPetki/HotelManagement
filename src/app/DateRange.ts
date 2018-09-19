7174b143287d8f88a2ff33649abae5cf502d7ef5
tamasPetki <tonertop@gmail.com>
2018-09-19 20:17:16 +0200

    Datepicker works, button preparing

---
+++ src/app/DateRange.ts
@@ -0,0 +1,4 @@
+export class DateRange {
+  startDate: string;
+  endDate: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import {MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material';
+import { MatFormFieldModule} from '@angular/material';
+import { MatNativeDateModule} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { DatepickercomplexComponent } from './datepickercomplex/datepickercomplex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatepickerComponent,
+    DatepickercomplexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
